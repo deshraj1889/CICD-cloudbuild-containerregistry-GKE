460001c5a70da1821745f44af9f4c8ba99320cff
deshraj1889 <57320191+deshraj1889@users.noreply.github.com>
2023-03-29 09:24:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hi python")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1')
